d6ba2e5faa1997cd7eb0edfb5a3ac193af8ebecb
Nomi Zis <Nomi7066@gmail.com>
2019-02-24 12:26:39 +0200

    add reproduce project

---
+++ IncorrectStringKey/Controllers/ItemsController.cs
@@ -0,0 +1,51 @@
+﻿using IncorrectStringKey.Dto;
+using Microsoft.AspNet.OData;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IncorrectStringKey.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemsController : ControllerBase
+    {
+
+        IEnumerable<Item> _items = new Item[] {
+                new Item() { Id="00000001",CompanyId="Company1",Description="description"},
+                new Item() { Id="00000002",CompanyId="Company2",Description="description"},
+                new Item() { Id="00000003",CompanyId="Company3",Description="description"},
+                new Item() { Id="00000004",CompanyId="Company4",Description="description"},
+            };
+        // GET api/values
+        [HttpGet]
+        public IQueryable<Item> Get()
+        {
+            return _items.AsQueryable();
+        }
+        // GET /items(CompanyId='Company1',Id='00000001')
+        public ActionResult<Item> Get([FromODataUri]string keyCompanyId, [FromODataUri]string keyId)
+        {
+            // The keyId is "1" not "00000001" as expectable
+            Item item = _items.FirstOrDefault(itm => itm.Id == keyId && itm.CompanyId == keyCompanyId);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return item;
+        }
+        //// GET /items('00000001')
+        //public ActionResult<Item> Get([FromODataUri]string key)
+        //{
+        //    Item item = _items.FirstOrDefault(itm => itm.Id == key);
+        //    if (item == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return item;
+        //}
+
+
+
+    }
+}
---
+++ IncorrectStringKey/Dto/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IncorrectStringKey.Dto
+{
+    public class Item
+    {
+        [Key]
+        public string Id { get; set; }
+       [Key]
+        public string CompanyId { get; set; }
+
+        public string Description { get; set; }
+    }
+}
